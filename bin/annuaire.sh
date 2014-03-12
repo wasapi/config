6a998ec1846e8a5783dceb4ca1a72e37ddb0ff4e
Yann Lambert <ylambert@ncnumericable.com>
2014-03-12 09:55:03 +0100

    script annuaire

--- bin/annuaire.sh
+++ bin/annuaire.sh
@@ -2,13 +2,14 @@
 
 utilisateur="<utilisateur>"
 mdp='<mot de passe>'
+serveur="<serveur>"
 # wget compilé avec support de NTLM
 wget_bin=${LOCAL_BIN}/wget/bin/wget
 
 nom=$(cat /dev/null | dmenu -b -p "Nom : ")
 
 IFS=$'\n'
-carte=( $(ldapsearch -x -h 172.16.204.45 -b 'dc=groupe,dc=int' -D "groupe\\${utilisateur}" -w ''${mdp}'' "(cn=${nom}*)" | awk -F': ' '/cn:|title:|mail:|mobile:|telephoneNumber:/{print $2}') )
+carte=( $(ldapsearch -x -h ${serveur} -b 'dc=groupe,dc=int' -D "groupe\\${utilisateur}" -w ''${mdp}'' "(cn=${nom}*)" | awk -F': ' '/cn:|title:|mail:|mobile:|telephoneNumber:/{print $2}') )
 nom_link=$(echo ${carte[O],,} | sed -e 's/[^ ]*/\u&/g' -e 's/ //g')
 img_link=$(${wget_bin} --user=${utilisateur} --password=${mdp} -qO - http://link/xwiki/bin/view/XWiki/${nom_link} | sed -n 's#.*nbglobalprofilFoto.*src="\(.*\)" border.*#http://link\1#p')
 
